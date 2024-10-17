8b030ffdccea15185ce7ae12603d0579ba583a77
Lenar <tyttrost1@gmail.com>
2024-10-17 14:43:24 +0500

    k

--- prog.c
+++ prog.c
@@ -4,7 +4,7 @@
 
 void fill(int n, int a[]){
     for (int i=0; i<n; i++){
-        a[i] = rand()%100+1;   
+        a[i] = rand()%7+1;   
     }
 }
 
@@ -19,21 +19,52 @@ int sravn(int n, int A[], int B[]){
     int sc=0,zap=0;
     for (int i=0; i<n; i++){
         for (int j=0; j<n; j++){
-            if (A[i]==B[j])
-                sc+=1;    
+            if (A[i]==B[j]){
+                for(int z=0; z<i; z++){
+                    if(A[z]==A[i]){
+                        zap+=1;
+                    }
+                } 
+                for(int z=0; z<j; z++){
+                    if(B[z]==B[j]){
+                        zap+=1;
+                    }
+                }     
+                if(zap>0){
+                    zap=0;
+                    break;
+                } else 
+                    sc+=1; 
+            }
         }
     }
     return sc;
 }
 
 void newc(int n, int a[], int b[]){
-    int C[sravn(n,a,b)],s=0;
+    int C[sravn(n,a,b)],s=0,zap=0;
+    printf("Kol. povtoreni %d\n",sravn(n,a,b));
     for (int i=0; i<n; i++){
         for (int j=0; j<n; j++){
             if (a[i]==b[j]){
-                C[s]=a[i];
-                s++; 
-            } 
+                for(int z=0; z<i; z++){
+                    if(a[z]==a[i]){
+                        zap+=1;
+                    }
+                } 
+                for(int z=0; z<j; z++){
+                    if(b[z]==b[j]){
+                        zap+=1;
+                    }
+                }     
+                if(zap>0){
+                    zap=0;
+                    break;
+                } else {
+                    C[s]=a[i]; 
+                    s+=1;
+                }
+            }
         }
     }
     printf("C:");
